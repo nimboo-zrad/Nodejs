b82e92d787fc351a78a735863d4c27da8ed45051
nima ziaei rad <nimazr800@gmail.com>
2025-04-02 23:48:35 +0330

    completed copy method!

--- fs/fileOperations/copy/copyFilePromise.js
+++ fs/fileOperations/copy/copyFilePromise.js
@@ -1,5 +1,23 @@
 import fs from 'fs';
 const fileName = './file';
-const destinationPath = './destination2';
-const secondDestinationPath = './destination2';
+const destinationPath = './destination3';
+const secondDestinationPath = './destination4';
 
+//async/await:
+
+async function copyMe(file, dest){
+  try{
+    await fs.promises.copyFile(file, dest);
+    console.log('file coppied sucessfully!');
+  }catch(err){
+    console.error('there has been an error: ', err.message);
+  }
+}
+
+copyMe(fileName, destinationPath);
+
+//then and catch methods:
+
+fs.promises.copyFile(fileName, secondDestinationPath)
+.then(()=> console.log('file has coppied!'))
+.catch(err => console.error('there is a problem!: ', err.message));
